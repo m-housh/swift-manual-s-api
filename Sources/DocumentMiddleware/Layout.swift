daf24abb7337763aea18c800dcfccaaeb3e6c9cb
m-housh <m-housh@users.noreply.github.com>
2023-02-10 14:30:51 +0000

    Ran swift-format

--- Sources/DocumentMiddleware/Layout.swift
+++ Sources/DocumentMiddleware/Layout.swift
@@ -9,7 +9,7 @@ func layout(title: String, content: Node) -> Node {
 
         .title(title),
 
-         .link(attributes: [
+        .link(attributes: [
           .rel(.stylesheet),
           .href("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"),
         ])
