3480df8b39ea4799a09bfc49d2b4fdc8da4d072e
Michael Housh <mhoush@houshhomeenergy.com>
2023-03-01 17:13:52 -0500

    wip

---
+++ Tests/CliMiddlewareTests/TemplateTests.swift
@@ -0,0 +1,64 @@
+import ClientConfigLive
+import CliMiddleware
+import Dependencies
+import FileClient
+import JsonDependency
+import LoggingDependency
+import TemplateClientLive
+import XCTest
+
+final class TemplateTests: XCTestCase {
+  
+  override func invokeTest() {
+    let (clientConfig, templateClient) = withDependencies {
+      $0.userDefaults = .temporary
+      $0.fileClient = .liveValue
+    } operation: {
+      (ConfigClient.liveValue, TemplateClient.liveValue)
+    }
+    
+    withDependencies {
+      $0.cliMiddleware = .liveValue
+      $0.configClient = clientConfig
+      $0.fileClient = .liveValue
+      $0.json = .liveValue
+      $0.templateClient = templateClient
+      $0.userDefaults = .temporary
+    } operation: {
+      super.invokeTest()
+    }
+  }
+  
+  func test_generating_and_removing_templates() async throws {
+    @Dependency(\.cliMiddleware) var cliMiddleware
+    
+    let tmp = FileManager.default.temporaryDirectory
+      .appendingPathComponent("template-test")
+    defer { try? FileManager.default.removeItem(at: tmp) }
+    
+    try await cliMiddleware.config(.set(tmp.absoluteString, for: .templatesDirectory))
+    
+    try await cliMiddleware.templates(.generate)
+    try await cliMiddleware.templates(.remove(force: true))
+  }
+  
+  func test_template() async throws {
+    @Dependency(\.cliMiddleware) var cliMiddleware
+    
+    let tmp = FileManager.default.temporaryDirectory
+      .appendingPathComponent("template-test")
+    defer { try? FileManager.default.removeItem(at: tmp) }
+    try FileManager.default.createDirectory(at: tmp, withIntermediateDirectories: true)
+    
+    try await cliMiddleware.templates(.template(key: .project, outputContext: .copy))
+    try await cliMiddleware.templates(.template(key: .project, outputContext: .echo))
+    try await cliMiddleware.templates(.template(key: .boiler, embedIn: .interpolation, outputContext: .write(to: tmp)))
+    try await cliMiddleware.templates(.template(key: .furnace, embedIn: .route, outputContext: .write(to: tmp)))
+    do {
+      try await cliMiddleware.templates(.template(key: .baseInterpolation, embedIn: .route, outputContext: .write(to: tmp)))
+      XCTFail()
+    } catch {
+      XCTAssert(true)
+    }
+  }
+}
--- Tests/CliMiddlewareTests/ValidationTests.swift
+++ Tests/CliMiddlewareTests/ValidationTests.swift
@@ -15,14 +15,14 @@ final class ValidationTests: XCTestCase {
   override func invokeTest() {
     let projectData = try! JSONEncoder().encode(Template.Project.mock)
     let fileClient = FileClient.mock(readData: projectData)
-    
+
     let configClient = withDependencies {
       $0.fileClient = fileClient
       $0.userDefaults = .temporary
     } operation: {
       ConfigClient.liveValue
     }
-    
+
     withDependencies {
       $0.configClient = configClient
       $0.json = .liveValue
@@ -40,8 +40,43 @@ final class ValidationTests: XCTestCase {
     } operation: {
       @Dependency(\.cliMiddleware) var cliMiddleware
       let tmp = URL(fileURLWithPath: "/some/where/that/is/not/there/because/we/mock/the/file/client")
+
       try await cliMiddleware.validate(.init(key: .project, inputFile: tmp))
     }
-    
+
   }
+  
+//  func test_all_validations() async throws {
+//    for key in Template.PathKey.allCases {
+//      if key != .baseInterpolation {
+//        let data = try! JSONEncoder().encode(key.mock)
+//        let fileClient = FileClient.mock(readData: data)
+//
+//        let configClient = withDependencies {
+//          $0.fileClient = fileClient
+//          $0.userDefaults = .temporary
+//        } operation: {
+//          ConfigClient.liveValue
+//        }
+//
+//        await withDependencies {
+//          $0.configClient = configClient
+//          $0.fileClient = fileClient
+//          $0.json = .liveValue
+//          $0.validationMiddleware = .liveValue
+//          $0.cliMiddleware = .liveValue
+//        } operation: {
+//          @Dependency(\.cliMiddleware) var cliMiddleware
+//          let tmp = URL(fileURLWithPath: "/foo/bar")
+//          do {
+//            try await cliMiddleware.validate(.init(key: key, inputFile: tmp))
+//          } catch {
+//            print("Internal error")
+//            print("\(error)")
+//          }
+//        }
+//
+//      }
+//    }
+//  }
 }
